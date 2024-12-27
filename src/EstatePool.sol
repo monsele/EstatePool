b35a19589f783cf3a529a188aa1a8cb34debf512
monsele <eronoaik@gmail.com>
2024-12-27 14:53:28 +0100

    Fix to currency token Id

--- src/EstatePool.sol
+++ src/EstatePool.sol
@@ -17,21 +17,20 @@ contract EstatePool is ERC1155, ERC1155Holder {
     AuctionData[] private auctions;
     Currency[] private CurrencyList;
     uint256 private tokenCounter;
-    uint256 private currencyCounter;
     uint256 private auctionCounter;
     /// @dev mapping of tokenId to amount sold
-    mapping(uint256 => uint256) public availableTokenAmount;
+    mapping(uint256 => uint256) private availableTokenAmount;
     ///@dev Mapping for tokenId -> tokendata
-    mapping(uint256 => TokenData) public tokenMapping;
+    mapping(uint256 => TokenData) private tokenMapping;
     /// @dev This refers to the user's total value bought
-    mapping(address => uint256) public userTvl;
+    mapping(address => uint256) private userTvl;
     /// @dev This is the user's total yields gained
-    mapping(address => uint256) public totalYields;
+    mapping(address => uint256) private totalYields;
     /// @dev This ties all the users to their respective tokens
     mapping(address => TokenData[]) private userTokens;
     /// @dev This mapping is for tracking Auctions
-    mapping(uint256 => AuctionData) public auction;
-    /// @dev this is a mapping of the byte representation of the short form
+    mapping(uint256 => AuctionData) private auction;
+    /// @dev this is a mapping of the byte representation of the currency code
     /// to the currency struct
     mapping(bytes32 => Currency) NameToFiat;
 
@@ -106,20 +105,25 @@ contract EstatePool is ERC1155, ERC1155Holder {
 
     /// function to list and provide tokens of an asset
     /// @param name The name of asset
-    /// @param totalPlots Total availiable plots
+    /// @param totalPlots Total available plots
     /// @param amtToSell Amount willing to see to investors
     /// @param estateType Estate Type enum
-    function CreateAsset(string memory name, uint256 totalPlots, uint256 amtToSell, EstateType estateType)
-        external
-        returns (TokenData memory)
-    {
-        tokenCounter = GetTokenCounter() + 1;
-        TokenData memory tokenData = TokenData(name, tokenCounter, msg.sender, totalPlots, amtToSell, estateType, false);
-        _mint(msg.sender, tokenCounter, totalPlots, "");
+    /// @param userAddress Estate Type enum
+    function CreateAsset(
+        string memory name,
+        uint256 totalPlots,
+        uint256 amtToSell,
+        EstateType estateType,
+        address userAddress
+    ) external returns (TokenData memory) {
+        tokenCounter = getTokenCounter() + 1;
+        TokenData memory tokenData =
+            TokenData(name, tokenCounter, userAddress, totalPlots, amtToSell, estateType, false);
+        _mint(userAddress, tokenCounter, totalPlots, "");
         ListedTokens.push(tokenData);
         availableTokenAmount[tokenCounter] = amtToSell;
         tokenMapping[tokenCounter] = tokenData;
-        _setApprovalForAll(msg.sender, address(this), true);
+        _setApprovalForAll(userAddress, address(this), true);
         emit TokenListed(tokenData.Owner, tokenData.Name, tokenData.Id);
         return tokenData;
     }
@@ -127,8 +131,8 @@ contract EstatePool is ERC1155, ERC1155Holder {
     function create(string memory name, string memory shortForm, string memory imageUri) external {
         bytes32 key = keccak256(abi.encodePacked(shortForm));
         require(NameToFiat[key].Id == 0, "Short form already exist");
-        currencyCounter = GetCurrencyTokenCounter() + 1;
-        Currency memory currency = Currency(currencyCounter, name, shortForm, imageUri);
+        tokenCounter = getTokenCounter() + 1;
+        Currency memory currency = Currency(tokenCounter, name, shortForm, imageUri);
         CurrencyList.push(currency);
 
         NameToFiat[key] = currency;
@@ -148,89 +152,123 @@ contract EstatePool is ERC1155, ERC1155Holder {
         _burn(user, currency.Id, amount);
     }
 
-    ///
     /// @param tokenId  this is the Id of the token on the ListedToken array
     /// @param purchaseAmt this is the amount of units that the user wants to purchase
     /// @param payAmount This is the expected amount the user should send to the smart contract
-    /// @return Id this holds the return value of the token that was bought
-    /// @return amountBought this holds the value of the token that was successfully bought
+    /// @param userAddress This is the address the paymaster is sponsoring and represent the user that made the call
+    /// @param currencyCode This is for the currency code of the payment token
     function BuyPlot(
         uint256 tokenId,
         uint256 purchaseAmt,
         uint256 payAmount,
         address userAddress,
-        string memory shortform
-    ) external payable returns (uint256 Id, uint256 amountBought) {
-        ///@notice expected pay should be the converted value of the eth price to wei as wei is the value of msg.value
-        // require(expectedPay >= msg.value, "The amount sent is not enough for purchase");
-
+        string memory currencyCode
+    ) external {
+        // 1. CHECKS
         TokenData memory data = tokenMapping[tokenId];
+        address recipient = data.Owner; // Added back the recipient address
         require(userAddress != data.Owner, "Owner Cannot buy listed property");
         uint256 availableAmt = availableTokenAmount[tokenId];
         require(purchaseAmt <= availableAmt, "Purchase amount exceeds the available amount");
-        address recipient = data.Owner;
 
-        bytes32 key = keccak256(abi.encodePacked(shortform));
+        bytes32 key = keccak256(abi.encodePacked(currencyCode));
         Currency memory currency = NameToFiat[key];
         if (currency.Id == 0) {
             revert EstatePool__CurrencyNotFound("Currency cannot be found");
         }
-        _safeTransferFrom(userAddress, recipient, currency.Id, payAmount, "0x");
 
-        _safeTransferFrom(recipient, userAddress, tokenId, purchaseAmt, "0x");
-
+        // 2. EFFECTS
         availableTokenAmount[tokenId] = availableTokenAmount[tokenId] - purchaseAmt;
         userTvl[userAddress] = userTvl[userAddress] + payAmount;
-        AddTokenToUser(userAddress, tokenId);
+        addTokenToUser(userAddress, tokenId);
+
+        // 3. INTERACTIONS
+        _safeTransferFrom(userAddress, recipient, currency.Id, payAmount, "0x");
+        _safeTransferFrom(recipient, userAddress, tokenId, purchaseAmt, "0x");
+
         emit TokenBought(recipient, userAddress, tokenId);
-        Id = tokenId;
-        amountBought = purchaseAmt;
     }
 
-    function AuctionAsset(uint256 tokenId, uint256 amount, address userAddress) external returns (bool, uint256) {
-        auctionCounter = GetAuctionCounter() + 1;
+    function AuctionAsset(uint256 tokenId, uint256 amount, address userAddress) external {
+        auctionCounter = getAuctionCounter() + 1;
         _safeTransferFrom(userAddress, address(this), tokenId, amount, "0x");
 
         auction[auctionCounter] = AuctionData(tokenId, amount, userAddress, auctionCounter, false);
         auctions.push(AuctionData(tokenId, amount, userAddress, auctionCounter, false));
         emit AuctionCreated(auctionCounter, userAddress, tokenId, amount);
-        return (true, auctionCounter);
+        //return (true, auctionCounter);
     }
 
-    function PayBid(uint256 auctionId, uint256 amount) external payable returns (bool) {
+    /// @param auctionId  this is the Id of the auction
+    /// @param amount This is the amount that you are paying in currency
+    /// @param userAddress This is the address the paymaster is sponsoring and represent the user that made the call
+    /// @param currencyCode This is for the currency code of the payment token
+    function PayBid(uint256 auctionId, uint256 amount, string memory currencyCode, address userAddress) external {
         AuctionData storage auctionData = auction[auctionId];
         require(!auctionData.completed, "Auction is already completed");
-        require(msg.value >= amount, "Invalid Amount");
-        address payable owner = payable(auctionData.Owner);
+        address owner = auctionData.Owner;
         uint256 amountToSell = auctionData.AmountToSell;
         uint256 tokenId = auctionData.TokenId;
-
-        // Transfer ETH to the owner
-        (bool success,) = owner.call{value: msg.value}("");
-        require(success, "ETH transfer failed");
+        bytes32 key = keccak256(abi.encodePacked(currencyCode));
+        Currency memory currency = NameToFiat[key];
+        if (currency.Id == 0) {
+            revert EstatePool__CurrencyNotFound("Currency cannot be found");
+        }
+        //Transfer the currency
+        _safeTransferFrom(userAddress, owner, currency.Id, amount, "0x");
 
         // Transfer tokens to the bidder
-        _safeTransferFrom(address(this), msg.sender, tokenId, amountToSell, "");
+        _safeTransferFrom(address(this), userAddress, tokenId, amountToSell, "");
 
         // Mark the auction as completed
         auctionData.completed = true;
 
         // Add token to user (make sure this function exists and works as expected)
-        AddTokenToUser(msg.sender, tokenId);
+        addTokenToUser(userAddress, tokenId);
 
-        emit AuctionPaid(msg.sender, owner, auctionId, msg.value);
-        return true;
+        emit AuctionPaid(userAddress, owner, auctionId, amount);
     }
 
-    function GetListedTokens() external view returns (TokenData[] memory) {
+    function getListedTokens() external view returns (TokenData[] memory) {
         return ListedTokens;
     }
+    /// @notice Get the available amount for a specific token
+    /// @param tokenId The ID of the token to query
+    /// @return Amount of tokens available
 
-    function GetAvailableTokenAmount(uint256 tokenId) external view returns (uint256) {
+    function getAvailableTokenAmount(uint256 tokenId) external view returns (uint256) {
         return availableTokenAmount[tokenId];
     }
 
-    function GetUserTokensData(address user) external view returns (UserTokenData[] memory) {
+    /// @notice Get token data for a specific token ID
+    /// @param tokenId The ID of the token to query
+    /// @return TokenData struct containing token information
+    function getTokenData(uint256 tokenId) external view returns (TokenData memory) {
+        return tokenMapping[tokenId];
+    }
+    /// @notice Get total value locked for a specific user
+    /// @param user Address of the user to query
+    /// @return Total value locked for the user
+
+    function getUserTvl(address user) external view returns (uint256) {
+        return userTvl[user];
+    }
+    /// @notice Get total yields for a specific user
+    /// @param user Address of the user to query
+    /// @return Total yields for the user
+
+    function getUserYields(address user) external view returns (uint256) {
+        return totalYields[user];
+    }
+    /// @notice Get auction data for a specific token
+    /// @param tokenId The ID of the token to query auction data for
+    /// @return AuctionData struct containing auction information
+
+    function getAuctionData(uint256 tokenId) external view returns (AuctionData memory) {
+        return auction[tokenId];
+    }
+
+    function getUserTokensData(address user) external view returns (UserTokenData[] memory) {
         TokenData[] memory userTokenData = userTokens[user];
         //TokenData[] memory userTokenData2 = userTokensss[user];
         UserTokenData[] memory userTokenInfo = new UserTokenData[](userTokenData.length);
@@ -243,7 +281,19 @@ contract EstatePool is ERC1155, ERC1155Holder {
         return userTokenInfo;
     }
 
-    function AddTokenToUser(address user, uint256 tokenId) public returns (bool) {
+    function getUserStats(address user) external view returns (
+    uint256 tvl,
+    uint256 yields,
+    TokenData[] memory tokens
+) {
+    return (
+        userTvl[user],
+        totalYields[user],
+        userTokens[user]
+    );
+}
+
+    function addTokenToUser(address user, uint256 tokenId) public returns (bool) {
         TokenData[] memory tokens = userTokens[user];
         for (uint256 i = 0; i < tokens.length; i++) {
             if (tokens[i].Id == tokenId) {
@@ -254,19 +304,14 @@ contract EstatePool is ERC1155, ERC1155Holder {
         return true;
     }
 
-    function GetTokenCounter() public view returns (uint256) {
+    function getTokenCounter() public view returns (uint256) {
         return tokenCounter;
     }
-
-    function GetCurrencyTokenCounter() public view returns (uint256) {
-        return currencyCounter;
-    }
-
-    function GetAuctionCounter() public view returns (uint256) {
+    function getAuctionCounter() public view returns (uint256) {
         return auctionCounter;
     }
 
-    function GetAuctions() external view returns (AuctionData[] memory) {
+    function getAuctions() external view returns (AuctionData[] memory) {
         return auctions;
     }
 
